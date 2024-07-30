e07982b3d40572ec1afc37f84385509059f17572
cozyat <cozyat@gmail.com>
2024-07-29 20:33:27 -0500

    Create hard_4.java

---
+++ hard_4.java
@@ -0,0 +1,31 @@
+// 4. Median of Two Sorted Arrays
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    private double median;
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        ArrayList<Double> doubleArrayList = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            doubleArrayList.add((double) nums1[i]);
+        }
+
+        for (int j = 0; j < nums2.length; j++) {
+            doubleArrayList.add((double) nums2[j]);
+        }
+
+        Collections.sort(doubleArrayList);
+
+        int size = doubleArrayList.size();
+        if (size % 2 == 0) {
+            median = (doubleArrayList.get(size / 2 - 1) +
+            doubleArrayList.get(size / 2)) / 2;
+        } else {
+            median = doubleArrayList.get(size / 2);
+        }
+
+        return median;
+    }
+}
