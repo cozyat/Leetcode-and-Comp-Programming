8667041036ed2807846ee6223336f80faffd4ff2
cozyat <cozyat@gmail.com>
2024-07-29 20:40:28 -0500

    Create easy_2469.py

--- easy_2469.py
+++ easy_2469.py
@@ -1,11 +1,6 @@
 # 2469. Convert the Temperature
 class Solution(object):
     def convertTemperature(self, celsius):
-        """
-        :type celsius: float
-        :rtype: List[float]
-        """
-        
         kelvin = celsius + 273.15
         fahrenheit = (celsius * 1.80) + 32.00
 
@@ -15,3 +10,8 @@ class Solution(object):
         result.append(fahrenheit)
 
         return result
+
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
