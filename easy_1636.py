ae89aa7e10b56d729af78831546c8c6b13993532
cozyat <cozyat@gmail.com>
2024-07-29 20:39:13 -0500

    Create easy_1636.py

--- easy_1636.py
+++ easy_1636.py
@@ -1,11 +1,11 @@
 # 1636. Sort Array by Increasing Frequency
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        count = Counter(nums) # Counter method: creates a hash map for nums list
+        count = Counter(nums)
 
         def custom_sort(n):
             return (count[n], -n)
 
-        nums.sort(key=custom_sort) # sort nums ascending
+        nums.sort(key=custom_sort)
 
         return nums
