763956bf3e5b47fdd24399bc98fdf9c23966b1f9
cozyat <cozyat@gmail.com>
2024-08-08 16:56:44 -0500

    Create easy_509.java

---
+++ easy_509.java
@@ -0,0 +1,11 @@
+// 509. Fibonacci Number
+public class Solution {
+    public static int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+      return fib(n - 1) + fib(n - 2);
+
+    }
+}
