3fef474ca0827616ca9c02366b010cfa74b69028
cozyat <cozyat@gmail.com>
2024-08-12 09:26:57 -0500

    Create easy_1108.java

---
+++ easy_1108.java
@@ -0,0 +1,15 @@
+// 1108. Defanging an IP Address
+public class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            char c = address.charAt(i);
+            if (c == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
