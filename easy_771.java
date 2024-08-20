f46fe9772d98c566f4202c1ed0dffe334f4257ae
cozyat <cozyat@gmail.com>
2024-08-19 21:54:05 -0500

    Create easy_771.java

---
+++ easy_771.java
@@ -0,0 +1,12 @@
+// 771. Jewels and Stones
+public class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.indexOf(stones.charAt(i)) > -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
