e1fec40e31839ff87f000077a78ac7ac06dacd3e
cozyat <cozyat@gmail.com>
2024-08-23 18:14:34 -0500

    Create easy_2695.java

---
+++ easy_2695.java
@@ -0,0 +1,15 @@
+// 2695. Array Wrapper
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+  this.array = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+  return this.array.reduce((sum, num) => sum + num, 0);
+};
+
+ArrayWrapper.prototype.toString = function() {
+  return "[" + this.array.join(",") + "]";
+};
