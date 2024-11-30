1526e7a37c911cef7da15dbdf10ebf106e3ecc52
cozyat <cozyat@gmail.com>
2024-11-29 18:27:29 -0600

    Create medium_229.java

---
+++ medium_229.java
@@ -0,0 +1,24 @@
+// 229. Majority Element II
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == nums[i]) {
+                    count++;
+                }
+            }
+          
+            if (count > n / 3 && !result.contains(nums[i])) {
+                result.add(nums[i]);
+            }
+        }
+        return result;
+    }
+}
