9b40f2021784cfbbd04a5665f8050203c3b630c2
cozyat <cozyat@gmail.com>
2024-11-29 18:30:07 -0600

    Create medium_200.java

--- medium_200.java
+++ medium_200.java
@@ -20,6 +20,7 @@ public class Solution {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
             return 0;
         }
+        
         grid[i][j] = '0';
         dfs(grid, i + 1, j);
         dfs(grid, i - 1, j);
