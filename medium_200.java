873f6af518b2e27c37631c658c8f32182e4a8f8d
cozyat <cozyat@gmail.com>
2024-11-29 18:29:51 -0600

    Update medium_200.java

--- medium_200.java
+++ medium_200.java
@@ -9,22 +9,22 @@ public class Solution {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == '1') {
-                    numIslands += depthFirstSearch(grid, i, j);
+                    numIslands += dfs(grid, i, j);
                 }
             }
         }
         return numIslands;
     }
 
-    public int depthFirstSearch(char[][] grid, int i, int j) {
+    public int dfs(char[][] grid, int i, int j) {
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
             return 0;
         }
         grid[i][j] = '0';
-        depthFirstSearch(grid, i + 1, j);
-        depthFirstSearch(grid, i - 1, j);
-        depthFirstSearch(grid, i, j + 1);
-        depthFirstSearch(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
         return 1;
     }
 }
