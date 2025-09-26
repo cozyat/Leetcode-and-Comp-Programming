732295f0bdfa211f08ad696f372ac5c176d416f1
Soham Panda <cozyat@gmail.com>
2025-09-26 14:11:53 -0500

    Create easy_1480.java

---
+++ easy_1480.java
@@ -0,0 +1,11 @@
+// 1480. Running Sum of 1D Array
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] results = new int[nums.length];
+        results[0] = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            results[i] = nums[i] + results[i - 1];
+        }
+        return results;
+    }
+}
