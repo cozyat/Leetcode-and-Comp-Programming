56cb741925e41e98ae0eecc06b5a980a24f145eb
Soham Panda <cozyat@gmail.com>
2025-09-30 17:19:07 -0500

    Update medium_2221.java

--- medium_2221.java
+++ medium_2221.java
@@ -9,7 +9,7 @@ public class Solution {
         }
     }
 
-    public int elementPresent(int[] nums, int n) {
+    private int elementPresent(int[] nums, int n) {
         if (n == 1) return nums[0];
         int[] newNums = new int[n - 1];
         for (int i = 0; i < newNums.length; ++i) {
