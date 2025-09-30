1050b1d1296f7d0ae7a3c5783e3be97965d0eda0
Soham Panda <cozyat@gmail.com>
2025-09-30 14:07:47 -0500

    Create medium_2221.java

---
+++ medium_2221.java
@@ -0,0 +1,25 @@
+// 2221. Find Triangular Sum of an Array
+public class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        } else {
+            return elementPresent(nums, n);
+        }
+    }
+
+    public int elementPresent(int[] nums, int n) {
+        if (n == 1) return nums[0];
+        int[] newNums = new int[n - 1];
+        for (int i = 0; i < newNums.length; ++i) {
+            newNums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        if (nums.length == newNums.length) {
+            for (int j = 0; j < nums.length; ++j) {
+                nums[j] = newNums[j];
+            }
+        }
+        return elementPresent(newNums, newNums.length);
+    }
+}
