64aef094d1e7a28bf9ddc63726eb3809e9189361
Soham Panda <cozyat@gmail.com>
2025-09-30 22:34:33 -0500

    Create easy_1518.java

---
+++ easy_1518.java
@@ -0,0 +1,6 @@
+// 1518. Water Bottles
+public class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles + (numBottles - 1) / (numExchange - 1);
+    }
+}
