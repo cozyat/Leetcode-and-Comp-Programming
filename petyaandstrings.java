cd10c8a0d39e8858f1f79c7fc1c28aa57b3e0b34
Soham Panda <cozyat@gmail.com>
2025-12-12 20:31:17 -0600

    Update petyaandstrings.java

--- petyaandstrings.java
+++ petyaandstrings.java
@@ -2,25 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int lex = 0;
+        Scanner sc = new Scanner(System.in); int lex = 0;
+        String L1 = sc.nextLine();
+        String L2 = sc.nextLine();
         
-        String lineOne = sc.nextLine().trim();
-        String lineTwo = sc.nextLine().trim();
-        
-        if (lineOne.length() == lineTwo.length()) {
-            for (int i = 0; i < lineOne.length(); i++) {
-                if (Character.toLowerCase(lineOne.charAt(i)) != Character.toLowerCase(lineTwo.charAt(i))) {
-                    if ((int) Character.toLowerCase(lineOne.charAt(i)) > (int) Character.toLowerCase(lineTwo.charAt(i))) {
-                        lex = 1;
-                        break;
-                    } else if ((int) Character.toLowerCase(lineOne.charAt(i)) < (int) Character.toLowerCase(lineTwo.charAt(i))) {
-                        lex = -1;
-                        break;
+        if (L1.length() == L2.length()) {
+            for (int i = 0; i < L1.length(); i++) {
+                if (check(L1, i) != check(L2, i)) {
+                    if (check(L1, i) > check(L2, i)) {
+                        lex += 1; break;
+                    } else if (check(L1, i) < check(L2, i)) {
+                        lex -= 1; break;
                     }
                 }
             }
         }
-        System.out.print(String.valueOf(lex));
+
+        System.out.print(lex); sc.close();
+    }
+
+    private static char check(String s, int n) {
+        return Character.toLowerCase(s.charAt(n));
     }
 }
